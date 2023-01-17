d0e60fcf70f7146aa3f51ba71676169ca19230fe
JordanWitt <wittjordan94@gmail.com>
2023-01-17 16:18:20 -0600

    added a redirect page dislaying guess, roll, and matches

--- src/main/java/com/codeup/blogapp/DiceRollController.java
+++ src/main/java/com/codeup/blogapp/DiceRollController.java
@@ -20,18 +20,18 @@ public class DiceRollController {
     @PostMapping("/roll-dice")
     public String rollDice(@RequestParam(name = "guess") int guess, Model model) {
         List<Integer> rolls = new ArrayList<>();
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             rolls.add(random.nextInt(6) + 1);
         }
         int matches = 0;
-        for(Integer roll : rolls){
-            if (roll == guess){
+        for (Integer roll : rolls) {
+            if (roll == guess) {
                 matches++;
             }
         }
         model.addAttribute("guess", guess + " This is what you guessed");
         model.addAttribute("rolls", rolls + " These are your rolls");
         model.addAttribute("matches", matches + " This is how many dice matches you have");
-        return "roll-dice";
+        return "roll-dice-n";
     }
 }
