00d63560c1942582caaaa593bf2124b95cec187a
JordanWitt <wittjordan94@gmail.com>
2023-01-17 11:23:29 -0600

    completed the controller exercises

---
+++ src/main/java/com/codeup/blogapp/MathController.java
@@ -0,0 +1,16 @@
+package com.codeup.blogapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+}
---
+++ src/main/java/com/codeup/blogapp/PostController.java
@@ -0,0 +1,29 @@
+package com.codeup.blogapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+        @RequestMapping(path = "/post", method = RequestMethod.GET)
+        @ResponseBody
+        public String viewPost() {
+            return " This is where the post index page would go";
+        }
+
+        @RequestMapping(path ="/post/{id}", method = RequestMethod.GET)
+        @ResponseBody
+        public String viewPostId(@PathVariable String id){
+            return "This is a post with an " + id;
+        }
+        @RequestMapping(path ="/posts/create", method = RequestMethod.GET)
+        @ResponseBody
+        public String viewCreatePost(){
+            return "This is a create page.";
+        }
+        @PostMapping(path ="/posts/create")
+        @ResponseBody
+        public String viewPostRequest(){
+            return "This is a post request of the create page/ create a new post";
+        }
+}
