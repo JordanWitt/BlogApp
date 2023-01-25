c5d415c808f129bc3a2aa614fa3f92550ab58fe4
JordanWitt <wittjordan94@gmail.com>
2023-01-25 09:44:07 -0600

    post test controller made

--- src/main/java/com/codeup/blogapp/controllers/PostTestController.java
+++ src/main/java/com/codeup/blogapp/controllers/PostTestController.java
@@ -1,4 +1,33 @@
 package com.codeup.blogapp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class PostTestController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String index(){
+        return "posts";
+    }
+
+    @GetMapping("/posts{id}")
+    @ResponseBody
+    public String indexId(@PathVariable long id){
+        return "posts/" + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String indexCreate(){
+        return "posts";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String indexShowCreate(){
+        return "posts/create";
+    }
 }
